fc962d7d1f0e436f634295087f7dd2fe8d4c20f8
anandskadari <anandskadari@gmail.com>
2020-02-16 15:39:05 +0530

    Initial AnswerController changes

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
@@ -0,0 +1,16 @@
+package com.upgrad.quora.api.controller;
+
+
+import com.upgrad.quora.api.model.AnswerEditResponse;
+import com.upgrad.quora.api.model.AnswerRequest;
+import com.upgrad.quora.api.model.AnswerResponse;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/answer")
+public class AnswerController {
+
+
+}
